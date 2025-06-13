0664930f05d2817ed79ec4d6286f2b542b2eb4da
Òscar Casajuana <elboletaire@gmail.com>
2025-06-13 11:59:58 +0200

    chore(build): 🧹 add clean-package to optimize package output and simplify exports
    This fixes the need to build the package after installing it via a non
    npm registry, but also requires anyone importing it via these methods to
    be using some sort of typescript compiler.
    
    What's done in the types export was required in order to have a coherent
    way to import types both before and after the bundling is done. So right
    now, all types are exported onto a single file, which is exported via
    the /types export. This is also helpful in case some type was used in
    both exports, since otherwise we were duplicating code.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import { createRequire } from 'module';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
+import { createRequire } from 'module';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
@@ -12,7 +12,7 @@ const pkg = require('./package.json');
 const createBundle = (config, options) => ({
   ...config,
   input: options.input,
-  external: Object.keys(pkg.dependencies || {}).filter(dep => 
+  external: Object.keys(pkg.dependencies || {}).filter(dep =>
     options.includeSnarkjs ? true : dep !== 'snarkjs'
   ),
 });
@@ -25,13 +25,13 @@ const createOutput = (name, options) => [
     file: `dist/${name}.umd.js`,
     format: 'umd',
     globals: {
-      'axios': 'axios',
+      axios: 'axios',
       '@vocdoni/davinci-contracts': 'davinciContracts',
-      'ethers': 'ethers',
-      'snarkjs': 'snarkjs',
-      '@ethereumjs/common': 'ethereumjsCommon'
-    }
-  }
+      ethers: 'ethers',
+      snarkjs: 'snarkjs',
+      '@ethereumjs/common': 'ethereumjsCommon',
+    },
+  },
 ];
 
 export default [
@@ -49,7 +49,36 @@ export default [
     input: 'src/index.ts',
     includeSnarkjs: true
   }),
-  
+
+  // Contracts bundle
+  createBundle(
+    {
+      plugins: [json(), commonjs(), resolve(), esbuild({ target: 'esnext' })],
+      output: createOutput('contracts', { umdName: 'ContractsSDK' }),
+    },
+    {
+      input: 'src/contracts/index.ts',
+      includeSnarkjs: true,
+    }
+  ),
+
+  // Sequencer bundle
+  createBundle(
+    {
+      plugins: [
+        json(),
+        commonjs(),
+        resolve({ browser: true }),
+        nodePolyfills(),
+        esbuild({ target: 'esnext' }),
+      ],
+      output: createOutput('sequencer', { umdName: 'SequencerSDK' }),
+    },
+    {
+      input: 'src/sequencer/index.ts',
+      includeSnarkjs: false,
+    }
+  ),
   // Main types bundle
   createBundle({
     plugins: [dts()],
