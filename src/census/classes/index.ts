8b80a365a4b703ee1d8469167567b2624674a96b
Marc Velmer <marc1987@gmail.com>
2025-10-22 11:35:21 +0200

    update census creation process to utilize new Census classes

--- src/census/classes/index.ts
+++ src/census/classes/index.ts
@@ -1,5 +1,6 @@
 export { Census, CensusType } from './Census';
 export { PlainCensus } from './PlainCensus';
-export { WeightedCensus, WeightedParticipant } from './WeightedCensus';
+export { WeightedCensus } from './WeightedCensus';
+export type { WeightedParticipant } from './WeightedCensus';
 export { CspCensus } from './CspCensus';
 export { PublishedCensus } from './PublishedCensus';
